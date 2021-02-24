abf18a902d7ff5ae357e50e437f1a576820bf2b9
Vivié Jérémy <jeremyvivie@hotmail.fr>
2021-02-24 16:09:25 +0100

    home et galerie photo/video

--- test/test.php
+++ test/test.php
@@ -1,36 +1,34 @@
 <?php
-include("../baseDeDonnee.php");
-
-$requete = "SELECT * FROM image ORDER BY date_poste DESC"; 
-$sql = $bdd -> query($requete);
-$dateRecent = $sql->fetch();
-$Recent = $dateRecent['date_poste'];
-
-$requete = "SELECT * FROM image ORDER BY date_poste"; 
-$sql = $bdd -> query($requete);
-$dateVieux = $sql->fetch();
-$Vieux = $dateVieux['date_poste'];
-
+require("../baseDeDonnee.php");
 
 $requete = "SELECT * FROM image ORDER BY date_poste DESC"; 
 $sql = $bdd -> prepare($requete);
 $sql->execute();
 
-// while  ($date = $sql->fetch())
-//     {
-//         echo $date['titre'];
-//         echo "<br/>";
-//     }
+$compteur=0;
 
-    for ($i = 0; $i < 4; $i++) {
-        $date = $sql->fetch();
+while($date = $sql->fetch())
+    {
         echo $date['titre'];
         echo "<br/>";
-        echo $date['description'];
-        echo "<br/>";
-        echo '<img src="'.$date['route_image'].'"/>';
-        echo "<br/>";
     }
+// echo $compteur;
+// $date = $sql->fetch();
+// var_dump($date);
+//     for ($i = 0; $i < $compteur; $i++) {
+//         $date = $sql->fetch();
+//         echo $date['route_image'];
+//     }
+
+    // for ($i = 0; $i < 4; $i++) {
+    //     $date = $sql->fetch();
+    //     echo $date['titre'];
+    //     echo "<br/>";
+    //     echo $date['description'];
+    //     echo "<br/>";
+    //     echo '<img src="'.$date['route_image'].'"/>';
+    //     echo "<br/>";
+    // }
 
 
 // echo $dateRecent['date_poste'];
