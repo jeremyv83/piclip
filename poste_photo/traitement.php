cd695e39a8da12e3fbe329a4d3c64fd64f445a3c
Vivié Jérémy <jeremyvivie@hotmail.fr>
2021-02-26 11:41:15 +0100

    merge

--- poste_photo/traitement.php
+++ poste_photo/traitement.php
@@ -32,21 +32,23 @@ if(isset($_POST['submit'])) {
         // Pour afficher la taille maximal accepter par le php.ini, il se trouve à wamp64/bin/php/
         // echo ini_get('upload_max_filesize'); 
         if($file_size <= 5000000) {
-            if(in_array($file_extension, $extension_autorise)){
-                $file_dest = '../poste/image/'.$file_name;
-                if(move_uploaded_file($file_tmp_name, $file_dest)){
-                    $req = $bdd->prepare("INSERT INTO image(id_user, titre, description, route_image, date_poste) VALUES (:id_user, :titre, :description, :file_dest, :date_poste)");
-                    $req->execute(array(
-                        'id_user'=> $id_user,
-                        'titre' => $titre,
-                        'description' => $description,
-                        'file_dest' => $file_dest,
-                        'date_poste' => $date_poste,
-                    ));
-                    
-                }
-            }else $return = "Seules les images en jpg et png sont autorisées.";
-            // Else est pareil que celui en bas car si $_FILES est vide c'est que le fichier est vraiment trop volumineux (plus de ennviron 25 mo)
+            if(strlen($description)<= 300){
+                if(in_array($file_extension, $extension_autorise)){
+                    $file_dest = '../poste/image/'.$file_name;
+                    if(move_uploaded_file($file_tmp_name, $file_dest)){
+                        $req = $bdd->prepare("INSERT INTO image(id_user, titre, description, route_image, date_poste) VALUES (:id_user, :titre, :description, :file_dest, :date_poste)");
+                        $req->execute(array(
+                            'id_user'=> $id_user,
+                            'titre' => $titre,
+                            'description' => $description,
+                            'file_dest' => $file_dest,
+                            'date_poste' => $date_poste,
+                        ));
+                        
+                    }
+                }else $return = "Seules les images en jpg et png sont autorisées.";
+            }else $return = "La description est limité à 300 caractères";
+                // Else est pareil que celui en bas car si $_FILES est vide c'est que le fichier est vraiment trop volumineux (plus de ennviron 25 mo)
         }else $return = "Attention la taille maximale des fichiers est de 5 mo";
     }else $return = "Fichier trop volumineux ! (5 mo maximium !)";
 }
