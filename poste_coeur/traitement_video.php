a2065d59ffd1be0f2da11efe94e85c2fd908116e
DorianLP <dorian.lepotier@gmail.com>
2021-02-28 17:23:50 +0100

    fin de projet

--- poste_coeur/traitement_video.php
+++ poste_coeur/traitement_video.php
@@ -1,7 +1,7 @@
 <?php
 
 if(isset($_SESSION) && isset($_POST['id_video_coeur']))
-$bdd->prepare("INSERT INTO coeur(id_coeur, id_user, id_video) VALUES ('', :id_user, :id_video)")
+$bdd->prepare("INSERT INTO coeur(id_user, id_video) VALUES (:id_user, :id_video)")
     ->execute(array(
       'id_user'=> $_SESSION["id"],
       'id_video'=> $_POST['id_video_coeur']
